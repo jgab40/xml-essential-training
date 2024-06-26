e4258f375045f576411419252130ad8fa169630d
jagb40 <jg.ndongendeba@gmail.com>
2024-06-26 20:26:40 +0200

    First commit

---
+++ Start/Ch5/script_AJAX XML Example.js
@@ -0,0 +1,9 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this)
+        }
+    }
+    xhttp.open("GET", "")
+}
